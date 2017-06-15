0a3de0c9381678a0bfce2cbf6a44f5c83e6feeac
Nathan Osman <nathan@quickmediasolutions.com>
2017-06-14 23:56:37 -0700

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM resin/rpi-raspbian
+MAINTAINER Nathan Osman <nathan@quickmediasolutions.com>
+
+RUN \
+    apt-get update && \
+    apt-get install -y python3 python3-pip && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN \
+    pip install influxdb w1thermsensor
+
+COPY tsat.py /root/tsat.py
+
+ENV \
+    INTERVAL=300 \
+    INFLUXDB_HOST=localhost \
+    INFLUXDB_PORT=8086 \
+    INFLUXDB_DATABASE=thsat \
+    INFLUXDB_USERNAME= \
+    INFLUXDB_PASSWORD= \
+    INFLUXDB_TAGS=
+
+CMD python /root/tsat.py \
+    --influxdb-host "$INFLUXDB_HOST" \
+    --influxdb-port "$INFLUXDB_PORT" \
+    --influxdb-database "$INFLUXDB_DATABASE" \
+    --influxdb-username "$INFLUXDB_USERNAME" \
+    --influxdb-password "$INFLUXDB_PASSWORD" \
+    --influxdb-tags "$INFLUXDB_TAGS"
---
+++ tsat.py
@@ -0,0 +1,53 @@
+from argparse import ArgumentParser
+from time import sleep
+
+from influxdb import InfluxDBClient
+from w1thermsensor import W1ThermSensor
+
+
+class Tsat:
+
+    def __init__(self, args):
+        self._client = InfluxDBClient(
+            args.influxdb_host,
+            args.influxdb_port,
+            args.influxdb_username,
+            args.influxdb_password,
+            args.influxdb_database,
+        )
+        self._sensor = W1ThermSensor()
+        self._interval = args.interval
+        self._tags = self._parse_tags(args.tags)
+
+    def run():
+        while True:
+            t = self._sensor.get_temperature()
+            self._client.write_points([
+                {
+                    'measurement': 'temperature',
+                    'tags': self._tags,
+                    'fields': {
+                        'value': t,
+                    },
+                },
+            ])
+            sleep(self._interval)
+
+    def _parse_tags(self, tagstr):
+        tags = {}
+        for p in tagstr.split(','):
+            for t in p.split('='):
+                tags[t[0]] = t[1]
+        return tags
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Read temperature data and push to InfluxDB")
+    parser.add_argument('--interval', default=300, type=int)
+    parser.add_argument('--influxdb-host', default='localhost')
+    parser.add_argument('--influxdb-port', default=8086, type=int)
+    parser.add_argument('--influxdb-database', default='thsat')
+    parser.add_argument('--influxdb-username')
+    parser.add_argument('--influxdb-password')
+    parser.add_argument('--influxdb-tags')
+    Tsat(parser.parse_args()).run()
